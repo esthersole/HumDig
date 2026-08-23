0417115acf8c1ca5e5328aaf9520a3ee086871e4
Esther <esthersole@users.noreply.github.com>
2026-08-23 17:13:27 +0200

    Update show-slides.sh

--- show-slides.sh
+++ show-slides.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Usage: ./show-slides.sh FILENAME.md"
+    echo "Usage: ./show-slides.sh lesson.md"
     exit 1
 fi
 
 echo "Opening $1..."
 
-python3 -m http.server 8000 >/dev/null 2>&1 &
-
-sleep 2
+if ! pgrep -f "http.server 8000" >/dev/null; then
+    python3 -m http.server 8000 >/dev/null 2>&1 &
+fi
 
 xdg-open "http://localhost:8000/slides.html?slides=$1"
